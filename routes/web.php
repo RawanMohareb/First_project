d0e6d4ac911ae33be0d82ea9db79f2ae916782e6
rawan.mohareb <rawan.mohareb@meti.dev>
2025-06-23 22:10:48 +0300

    themes added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ThemeController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(ThemeController::class)->name('theme.')->group(function(){
+    Route::get('/','index')->name('index');
+    Route::get('/contact','contact')->name('contact');
+    Route::get('/category','category')->name('category');
+    Route::get('/single_blog','single_blog')->name('single_blog');
+    Route::get('/login','login')->name('login');
+    Route::get('/register' ,'register')->name('register');
+
 });
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
